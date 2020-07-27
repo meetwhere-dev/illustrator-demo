fe21bdbcfd9dae35119673201e7341eb4c5d722f
wanyunfeng <wanyunfeng@ai-pattern.com>
2020-07-27 17:44:11 +0800

    已完成 直线和曲线工具

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,125 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+     <!-- 工具栏按钮  -->
+      <button
+      :key="index"
+      v-for="(item, index) of tools"
+      :class="activeTool === item ? 'active': ''"
+      @click="activeTool = item; applicationClick(item.toLowerCase())"
+      >{{item}}</button>
     </div>
-    <router-view/>
+    <!-- 画布 -->
+    <canvas id="c" width="800px" height="500px"></canvas>
   </div>
 </template>
+<script>
+import { fabric } from 'fabric'
+
+export default {
+  data () {
+    return {
+      // 工具栏
+      tools: ['Hand', 'BezierCurve', 'Line', 'Detect', 'Delete'],
+      // 选中的工具
+      activeTool: 'Hand',
+      // 画布
+      canvas: 'asdasdasd',
+      mouseCircle: null,
+      items: [],
+      createLine: {
+        // 第一次点击flag 确定newLine的第一点
+        isClick: false,
+        newLine: null
+      }
+    }
+  },
+  methods: {
+    // 对象内容作为方法名调用
+    applicationClick (methodsName) {
+      this.$options.methods[methodsName].apply(this)
+    },
+    hand () {
+      console.log('hand')
+    },
+    beziercurve () {
+      console.log('bezierCurve')
+    },
+    line () {
+      console.log('line')
+      console.log(this.canvas)
+    },
+    detect () {
+      console.log('detect')
+    },
+    delete () {
+      console.log('delete')
+    }
+  },
+  mounted () {
+    console.log(this.canvas)
+    function onObjectMoving (e) {
+      // const t = e.target
+      // console.log(t.left)
+    }
+    const canvas = new fabric.Canvas('c')
+    this.canvas = canvas
+    canvas.on({
+      'object:moving': onObjectMoving,
+      'mouse:move': (e) => {
+        circle.set({
+          left: e.absolutePointer.x - 10,
+          top: e.absolutePointer.y - 10
+        })
+        if (this.createLine.isClick) {
+          this.createLine.newLine.set({
+            x2: e.absolutePointer.x,
+            y2: e.absolutePointer.y
+          })
+        }
+        canvas.renderAll(circle)
+      },
+      'mouse:down': (e) => {
+        if (this.activeTool === 'Line') {
+          if (!this.createLine.isClick) {
+            const newLine = this.createLine.newLine = new fabric.Line([e.absolutePointer.x, e.absolutePointer.y, e.absolutePointer.x, e.absolutePointer.y], {
+              left: e.absolutePointer.x,
+              top: e.absolutePointer.y,
+              stroke: 'red',
+              selectable: false
+            })
+            this.canvas.add(newLine)
+          }
+          this.createLine.isClick = !this.createLine.isClick
+        }
+      }
+    })
+    const line = new fabric.Path('M 100 100 Q 200, 200, 300, 100', {
+      fill: '#fff',
+      stroke: 'black',
+      strokeWidth: 2
+    })
+    const circle = this.mouseCircle = new fabric.Circle({
+      left: -20,
+      top: 0,
+      radius: 10,
+      fill: '#fff',
+      stroke: '#666',
+      strokeWidth: 1
+    })
+    canvas.add(line)
+    canvas.add(circle)
+    console.log(canvas)
+  }
+}
+
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+canvas{
+  border:1px solid black
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.active{
+  background-color: hsl(200,50%,50%);
 }
 </style>
